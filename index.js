695e0826dbac06820479e8a3110cb06e120d3a17
Abdullah ElMetwali <133114413+abdullahelmetwali@users.noreply.github.com>
2023-12-20 05:07:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ let starsBox = document.querySelectorAll(".stars")
 let tstBox = document.querySelectorAll(".tstBox")
 let arrowRight = document.querySelector("[alt='arrowRight']")
 let arrowLeft = document.querySelector("[alt='arrowLeft']")
+let barsBox = document.querySelector(".barsBox")
 
-// Adding Star Comment
+// Mobile Menu Bar
+barsBox.addEventListener("click",function(){
+    let barOne = document.querySelector(".bars")
+    let barTwo = document.querySelector(".bars-stragged")
+    let navLinks = document.querySelector(".navLinks")
+        barOne.classList.toggle("none")
+        navLinks.classList.toggle("visible")
+        barTwo.classList.toggle("block")
+})
 
+// Adding Stars
 for(i = 0; i < starsBox.length; i++){
     for(k = 0; k < 5; k++){
         let img = document.createElement("img")
@@ -14,18 +24,24 @@ for(i = 0; i < starsBox.length; i++){
     }
 }
 
-// Making Arrow Comment
-
+// Arrow Of Comments
+console.log(tstBox.item(0).previousElementSibling)
 arrowLeft.addEventListener("click",function(){
-    tstBox.forEach((e)=>{
-        if(e.classList.contains("seen")){
-            e.classList.toggle("seen")
-        }else{
-            e.classList.toggle("seen")
-        }
-    })
+    tstBox.item(0).classList.add("seen")
+    tstBox.item(1).classList.remove("seen")
+    
+    if(tstBox.item(2).classList.contains("seen")){
+        tstBox.item(1).classList.add("seen")
+        tstBox.item(0).classList.remove("seen")
+        tstBox.item(2).classList.remove("seen")
+    }
 })
 arrowRight.addEventListener("click",function(){
-    tstBox.item(1).nextElementSibling.classList.toggle("seen")
-    tstBox.item(1).classList.toggle("seen")
+    tstBox.item(1).nextElementSibling.classList.add("seen")
+    tstBox.item(1).classList.remove("seen")
+    if(tstBox.item(0).classList.contains("seen")){
+        tstBox.item(0).classList.remove("seen")
+        tstBox.item(1).classList.add("seen")
+        tstBox.item(2).classList.remove("seen")
+    }
 })
